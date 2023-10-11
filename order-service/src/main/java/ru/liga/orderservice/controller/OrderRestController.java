fb07da2d467dc116e51c1016cada42e2098a72fd
VladShag <vladnnovgorod@yandex.ru>
2023-10-11 21:18:25 +0300

    OrderResponse added

--- order-service/src/main/java/ru/liga/orderservice/controller/OrderRestController.java
+++ order-service/src/main/java/ru/liga/orderservice/controller/OrderRestController.java
@@ -10,16 +10,16 @@ import java.util.List;
 public class OrderRestController {
     private final OrderService service = new OrderService();
     @GetMapping("/orders")
-    public List<Order> getAllOrders() {
-        return service.getAllOrders();
+    public OrderResponse getAllOrders() {
+        return new OrderResponse(service.getAllOrders());
     }
     @GetMapping("/order/{id}")
     public Order getOrderById(@PathVariable("id") long id) {
         return service.getOrderById(id);
     }
     @GetMapping("/order")
-    public List<Order> getOrdersByStatus(@RequestParam("status") String status){
-        return service.getOrdersByStatus(status);
+    public OrderResponse getOrdersByStatus(@RequestParam("status") String status){
+        return new OrderResponse(service.getOrdersByStatus(status));
     }
     @PostMapping("/order")
     public Order addNewOrder(@RequestBody Order order) {
@@ -28,5 +28,39 @@ public class OrderRestController {
     @PutMapping("/order/{id}")
     public Order changeOrderInfo(@PathVariable("id") long id, @RequestBody Order order) {
         return service.changeOrderInfo(id, order);
+    }
+    private class OrderResponse { ///Класс добавленный для корректного отображения в JSON данных в том формате,
+                                ///который указан в материалах
+        private List<Order> orders;
+        private int page_index;
+        private int page_count;
+        OrderResponse(List<Order> orders) {
+            this.orders = orders;
+            this.page_count = 10; //По умолчанию значение- 10, после более плотной работы с бд данный показатель изменится
+        }
+
+        public List<Order> getOrders() {
+            return orders;
+        }
+
+        public void setOrders(List<Order> orders) {
+            this.orders = orders;
+        }
+
+        public int getPage_index() {
+            return page_index;
+        }
+
+        public void setPage_index(int page_index) {
+            this.page_index = page_index;
+        }
+
+        public int getPage_count() {
+            return page_count;
+        }
+
+        public void setPage_count(int page_count) {
+            this.page_count = page_count;
+        }
     }
 }
