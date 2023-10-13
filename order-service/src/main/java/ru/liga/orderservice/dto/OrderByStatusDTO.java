f6750618cdea1273fd8fbfd571d542fea3ae81bd
VladShag <vladnnovgorod@yandex.ru>
2023-10-13 15:13:47 +0300

    OrderByStatusDTO fixed

--- order-service/src/main/java/ru/liga/orderservice/dto/OrderByStatusDTO.java
+++ order-service/src/main/java/ru/liga/orderservice/dto/OrderByStatusDTO.java
@@ -1,7 +1,11 @@
 package ru.liga.orderservice.dto;
 
-import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
+@Getter
+@Setter
 public class OrderByStatusDTO implements OrderDTOInt {
     private long id;
     private List<ItemToAddDTO> menuItems;
