03548a8c5ddab02ac374b1a074fff247f8da5724
VladShag <vladnnovgorod@yandex.ru>
2023-10-13 21:46:24 +0300

    insert into table added

---
+++ migration/src/main/resources/db/changelog/v.1.0.1/insert-into-customers.sql
@@ -0,0 +1,5 @@
+INSERT INTO customers(phone, email, address)
+VALUES ('8-999-888-77-66', 'myemail@yandex.ru', 'Nizhniy Novgorod, Restaurants st., 5'),
+       ('8-831-265-33-58', 'myworld@mail.ru', 'Nizhniy Novgorod, B.Pokrovskaya st., 79'),
+       ('8-953-753-41-23', 'badmail', 'Moscow, Some st., 32')
+;
---
+++ migration/src/main/resources/db/changelog/v.1.0.1/insert-into-restaurants.sql
@@ -0,0 +1,5 @@
+INSERT INTO restaurants(address, status)
+VALUES ('Nizhniy Novgorod, Restaurants st., 5', 'active'),
+       ('Nizhniy Novgorod, B.Pokrovskaya st., 79', 'active'),
+       ('Moscow, Some st., 32', 'not active')
+;
