a8adab74509b0eff78f873e47d54af51dacfc555
VladShag <vladnnovgorod@yandex.ru>
2023-10-13 15:15:22 +0300

    update ItemToShowCustomer

--- order-service/src/main/java/ru/liga/orderservice/dto/ItemToShowCustomerDTO.java
+++ order-service/src/main/java/ru/liga/orderservice/dto/ItemToShowCustomerDTO.java
@@ -1,5 +1,10 @@
 package ru.liga.orderservice.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ItemToShowCustomerDTO {
     private int price;
     private int quantity;
